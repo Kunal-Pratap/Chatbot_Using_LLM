69c31621af6c438fd7604af9eb725888fa7c9734
Kunal <Kunalpratappatil1455@gmail.com>
2024-06-30 10:28:07 +0530

    update

---
+++ research/tele_echo_bot.py
@@ -0,0 +1,22 @@
+import logging 
+from aiogram import Bot,Dispatcher, executor,types
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_TOKEN = os.getenv("TOKEN")
+
+# Configure Logging
+logging.basicConfig(level = logging.INFO)
+
+# Initialize bot and dispacher
+bot = Bot(token = API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start','help'])
+async def command_start_handler(message: types.Message):
+    await message.reply("Hi\nI am Echo Bot!\nPowered by aiogram.")
+    
+    
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
